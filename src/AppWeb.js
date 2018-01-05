6ab256a211146b477302b67ee681c8fd4c4aab20
Adan De Santiago Avalos <adan1995a@gmail.com>
2018-01-04 21:01:25 -0600

    panel graphic design done

--- src/AppWeb.js
+++ src/AppWeb.js
@@ -7,12 +7,47 @@ import './AppWeb.css';
 import SitiosHistoricos from './SitiosHistoricos.js';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
+import Icon from 'react-icons-kit';
+import { map_ok } from 'react-icons-kit/ikons/map_ok';
+import { location } from 'react-icons-kit/icomoon/location';
+import { feed} from 'react-icons-kit/icomoon/feed';
 
 
 
 
 class ApplicacionWeb extends Component {
+constructor(){
+  super()
+  this.state={
+    uno:'beacons',  //activo por defecto
+    dos:'lugaresInactivo',
+    tres:'historicosInactivo'
+  }
+}
+  cambioBeacons =()=>{
+    this.setState({
+      uno:'beacons',
+      dos:'lugaresInactivo',
+      tres:'historicosInactivo'
+    })
 
+ };
+ cambioLugares =()=>{
+   this.setState({
+     uno:'beaconsInactivo',
+     dos:'lugares',
+     tres:'historicosInactivo'
+   })
+
+};
+cambioHistoricos =()=>{
+  this.setState({
+    uno:'beaconsInactivo',
+    dos:'lugaresInactivo',
+    tres:'historicos'
+  })
+
+};
   render() {
     return (
 
@@ -21,11 +56,28 @@ class ApplicacionWeb extends Component {
           <div id="barraLateral">
 
                   <ul id='lista'>
-                    <li><Link to="/AppWeb"><p className='navOptions'>Beacons Activos</p></Link></li>
-                    <li><Link to="/AppWeb/Lugares"><p className='navOptions'>Lugares </p></Link></li>
-                    <li><Link to="/AppWeb/SitiosHistoricos"><p className='navOptions'>Sitios Historicos</p></Link></li>
+                  <div className='first'>
+                    <li></li>
+                  </div>
+                    <div className={this.state.uno}>
+                      <div className='icono'>
+                        <Icon size={45} icon={feed}  style={{ color: 'white' }}/>
+                      </div>
+                      <li onClick={this.cambioBeacons}><Link to="/AppWeb"><p className='navOptions'>Beacons Activos</p></Link></li>
+                    </div>
+                    <div className={this.state.dos}>
+                    <div className='icono'>
+                      <Icon size={45} icon={map_ok}  style={{ color: 'white' }}/>
+                    </div>
+                      <li onClick={this.cambioLugares}><Link to="/AppWeb/Lugares"><p className='navOptions'>Lugares </p></Link></li>
+                    </div>
+                    <div className={this.state.tres}>
+                    <div className='icono'>
+                      <Icon size={45} icon={location}  style={{ color: 'white' }}/>
+                    </div>
+                    <li onClick={this.cambioHistoricos}><Link to="/AppWeb/SitiosHistoricos"><p className='navOptions'>Sitios Historicos</p></Link></li>
+                    </div>
                   </ul>
-
           </div>
                   <div className="contenidoComponente">
                     <Route exact path="/AppWeb" component={BeaconsActivos} />
